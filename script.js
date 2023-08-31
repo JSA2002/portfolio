6b73493c8782a8894d55a9e71ff4d46e87ae193d
JSA2002 <jeetagrawal2002@gmail.com>
2023-08-31 17:32:36 +0530

    Third commit

--- script.js
+++ script.js
@@ -6,3 +6,46 @@ menuicon.onclick = () =>{
     menuicon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+/* scroll active class */
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+window.onscroll=()=>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*= ' + id + ']').classList.add('active');
+            });
+        };
+    })
+}
+
+
+/* about section */
+let more = document.querySelector("#rmore");
+let less = document.querySelector("#rless");
+
+more.onclick =()=>{
+    document.querySelector("#readmore").style.display = "block";
+    more.style.display = "none";
+    less.style.display = "block"
+}
+less.onclick =()=>{
+    document.querySelector("#readmore").style.display = "none";
+    more.style.display = "block";
+    less.style.display = "none"
+}
+
+/* skills reload */
+skills = document.querySelector("#skills");
+skills.onclick=()=>{
+   // skills.reload();
+}
+
+/* scroll page auto */
