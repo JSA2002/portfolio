b7c951680896f81cb768a5616878938535d5468e
JSA2002 <jeetagrawal2002@gmail.com>
2023-09-03 20:08:23 +0530

    Resume added + issue solved

--- script.js
+++ script.js
@@ -2,28 +2,28 @@
 let menuicon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuicon.onclick = () =>{
-    menuicon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+menuicon.onclick = () => {
+  menuicon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 }
 
 /* scroll active class */
-let sections = document.querySelectorAll('section');
+let sectionNav = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
-window.onscroll=()=>{
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+window.onscroll = () => {
+  sectionNav.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*= ' + id + ']').classList.add('active');
-            });
-        };
-    })
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*= ' + id + ']').classList.add('active');
+      });
+    };
+  })
 }
 
 
@@ -31,21 +31,28 @@ window.onscroll=()=>{
 let more = document.querySelector("#rmore");
 let less = document.querySelector("#rless");
 
-more.onclick =()=>{
-    document.querySelector("#readmore").style.display = "block";
-    more.style.display = "none";
-    less.style.display = "block"
+more.onclick = () => {
+  document.querySelector("#readmore").style.display = "block";
+  more.style.display = "none";
+  less.style.display = "block"
 }
-less.onclick =()=>{
-    document.querySelector("#readmore").style.display = "none";
-    more.style.display = "block";
-    less.style.display = "none"
+less.onclick = () => {
+  document.querySelector("#readmore").style.display = "none";
+  more.style.display = "block";
+  less.style.display = "none"
 }
 
 /* skills reload */
-skills = document.querySelector("#skills");
-skills.onclick=()=>{
-   // skills.reload();
-}
-
-/* scroll page auto */
+skillsClass = document.querySelectorAll(".skills-main .skills-bar .bar span");
+const skillsArray = ['html', 'css', 'js', 'react', 'bootstrap', 'git', 'mongodb', 'node', 'linux'];
+let i = 0;
+window.addEventListener('scroll', () => {
+  scrollPosition = window.scrollY;
+  windowHeight = window.innerHeight;
+  if (scrollPosition > windowHeight * 3) {
+    skillsClass.forEach((sClass) => {
+      sClass.classList.add(skillsArray[i++]);
+      //skillsArray.shift();
+    })
+  }
+});
